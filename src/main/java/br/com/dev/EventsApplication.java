fe5cee00b467edc10bba333a41ed113350e29faf
= <=>
2019-05-03 13:50:14 -0300

    ¨teste¨

--- src/main/java/br/com/dev/EventsApplication.java
+++ src/main/java/br/com/dev/EventsApplication.java
@@ -3,6 +3,7 @@ package br.com.dev;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//main
 @SpringBootApplication
 public class EventsApplication {
 
